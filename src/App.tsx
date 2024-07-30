eef2638c693275a1861f74a2bb8d87ad95dcf4d9
JosefKorba <josef.korba1@gmail.com>
2024-07-30 20:34:58 +0200

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 
 function App() {
-  
   return (
     <>
-      <NavBar />
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        gridTemplateColumns={{
+          base: "1fr",
+          lg: "250px 1fr",
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar />
+        </GridItem>
+        <Show above="lg">
+          <GridItem bg="blue.300" area="aside">
+            ASIDE
+          </GridItem>
+        </Show>
+        <GridItem bg="green.300" area="main">
+          MAIN
+        </GridItem>
+      </Grid>
     </>
   );
 }
